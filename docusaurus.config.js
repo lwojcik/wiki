f49abc8e23b792ea190711ed89d668c1943ac262
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-09-23 22:06:54 +0200

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,20 +23,20 @@ const config = {
         toExtensions: ["exe", "zip"], // /myAsset -> /myAsset.zip (if latter exists)
         redirects: [
           {
-            to: "/docs/social-media/t2",
-            from: ["/docs/social-media/pebble"],
+            from: ["/docs/social-media/t2"],
+            to: "/docs/social-media/pebble",
           },
         ],
-        createRedirects(existingPath) {
-          if (existingPath.includes("/community")) {
-            // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-            return [
-              existingPath.replace("/community", "/docs/team"),
-              existingPath.replace("/community", "/docs/support"),
-            ];
-          }
-          return undefined; // Return a falsy value: no redirect created
-        },
+        // createRedirects(existingPath) {
+        //   if (existingPath.includes("/community")) {
+        //     // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
+        //     return [
+        //       existingPath.replace("/community", "/docs/team"),
+        //       existingPath.replace("/community", "/docs/support"),
+        //     ];
+        //   }
+        //   return undefined; // Return a falsy value: no redirect created
+        // },
       },
     ],
   ],
